0bd3c0d7a776583ba6bf2111a7e16fd964410ef7
Andre Zimpel <andre@unknownbasics.com>
2021-07-29 20:21:50 +0200

    updated video

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -7,7 +7,7 @@ const Video = () => {
     <div className={wrapper}>
       <div className={container}>
         <div className={video}>
-          <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/pExywtT_KeY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/pExywtT_KeY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
       </div>
     </div>
