e9c9e0891528402839258e5491e149f676038188
Andre Zimpel <andre@unknownbasics.com>
2021-07-29 20:21:15 +0200

    updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,14 @@
-import * as React from "react"
-
 import Layout from "../components/layout"
+import React from "react"
 import Seo from "../components/seo"
+import Text from '../components/text'
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Seo title="Seite nicht gefunden" />
+    <Text>
+      Sorry, aber die Seite existiert nicht.
+    </Text>
   </Layout>
 )
 
